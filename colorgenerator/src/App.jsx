982b7c99a43fb706b4ac551024b56d038c1eddd6
Nikhil <nikhilgupta932003@gmail.com>
2024-08-01 02:04:46 +0530

    added the script

--- colorgenerator/src/App.jsx
+++ colorgenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,13 +7,43 @@ function App() {
   const [typeOfColor, setTypeOfColor] = useState("hex")
   const [color,setcolor]= useState("#000000");
 
+  function ramdomutility(length){
+    return Math.floor(Math.random()*length)
+  }
+
+  function handleCreateRandomHexColor(){
+    const hex=[1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+    let hexColor="#";
+    for(let i=0;i<6;i++){
+        hexColor+=hex[ramdomutility(hex.length)];
+    }
+  //  console.log(hexColor);
+setcolor(hexColor)
+  }
+
+  function handleCreateRandomRgbColor(){
+    const r=ramdomutility(256);
+    const g=ramdomutility(256);
+    const b=ramdomutility(256);
+    setcolor(`rgb${r},${g},${b}`)
+    console.log(`rgb${r},${g},${b}`);
+    }
+
+    useEffect(()=>{
+      if(typeOfColor==="rgb")handleCreateRandomRgbColor();
+      else handleCreateRandomHexColor();
+    },[typeOfColor]);
+   
+
+
+
   return (
     <>
     <div
     style={{width:"100vw",height:"100vh",background:color}}>
-      <button>Create Hex Color</button>
-      <button>Create RBG Color</button>
-      <button>Generate Ramdom Color</button>
+      <button onClick={()=>setTypeOfColor('hex')}>Create Hex Color</button>
+      <button onClick={()=>setTypeOfColor('rgb')}>Create RBG Color</button>
+      <button onClick={typeOfColor==='hex' ? handleCreateRandomHexColor : handleCreateRandomRgbColor}>Generate Ramdom Color</button>
     </div>
     </>
   )
